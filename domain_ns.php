e9e9ab82efa8edc56b50b53729887e0be81d75dc
Cha0s <cha0s@cha0s.awmn>
2014-08-11 05:13:54 +0300

    minor fixes

--- domain_ns.php
+++ domain_ns.php
@@ -167,6 +167,7 @@ if ($_POST['action'] == "add" ) {
 			$DOMAIN_parts = explode("." ,$DOMAIN['name']);
 			
 			$DOMAIN_parts[0] = false;
+			$DOMAIN_parts[1] = false;
 			$TLD = implode(".", $DOMAIN_parts);
 			$TLD =  substr($TLD, 1);			
 			
@@ -245,6 +246,7 @@ if ($_POST['action'] == "edit" && $_POST['id']) {
 			$DOMAIN_parts = explode("." ,$DOMAIN['name']);
 			
 			$DOMAIN_parts[0] = false;
+			$DOMAIN_parts[1] = false;
 			$TLD = implode(".", $DOMAIN_parts);
 			$TLD =  substr($TLD, 1);			
 			
