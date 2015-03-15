fa490739d32d9258c04cf3026fb88419d405138e
Cha0s <cha0s@cha0s.awmn>
2015-03-15 07:19:14 +0200

    fix total records

--- domains.php
+++ domains.php
@@ -94,7 +94,7 @@ $sort_vars = "&sort=".$_GET['sort']."&by=".$_GET['by'];
 
 // Paging
 $count = mysql_query("SELECT id FROM ".$mysql_table." ".$search_query,$db);
-$items_number  = mysql_num_rows($count) - 1; // minus 1 because we hide meta.meta zone
+$items_number  = mysql_num_rows($count);
 if ($_GET['items_per_page'] && is_numeric($_GET['items_per_page'])){
     $_SESSION['items_per_page'] = $_GET['items_per_page'];
 }
